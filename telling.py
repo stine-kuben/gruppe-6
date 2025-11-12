15c2a432b42a0947a3e4613b657ff12cde41d4d6
n00b-cat <alexanderstark@outlook.com>
2025-11-12 10:04:41 +0100

    oppgave 1 ඞඞඞඞඞඞ

--- telling.py
+++ telling.py
@@ -1,1 +1,11 @@
+def HvorMangeඞ(tekst, symbol):
+    AmongUsMeter = 0
+    
+    for letter in tekst:
+        if letter == symbol:
+            AmongUsMeter += 1
+    
+    print(f'Det er {AmongUsMeter} {symbol} i teksten: \n"{tekst}"')
 
+word = "dette er en ඞ tekst ඞ fr ඞ"
+HvorMangeඞ(word, "ඞ")
