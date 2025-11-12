b4acb65fd85eba6721d595930a5990ddc0f5b828
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-11-12 09:44:37 +0100

    dobletallliste

--- telling.py
+++ telling.py
@@ -1,1 +1,28 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def DobleTalIListe(tall:list):
+    print(tall)
+    dobleTall = []
+    for i in tall:
+        i = i*2
+        dobleTall.append(i)
+    print(dobleTall)
+DobleTalIListe([1,3,5])
+    
